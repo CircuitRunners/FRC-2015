40d8bbf137cdec88f0e4f70c46bcc6c17eb54e26
akilan98 <akilan98@gmail.com>
2015-02-11 17:24:57 -0500

    Removed encoder move

--- src/org/usfirst/frc/team1002/robot/OI.java
+++ src/org/usfirst/frc/team1002/robot/OI.java
@@ -1,28 +1,26 @@
 package org.usfirst.frc.team1002.robot;
 
 import org.usfirst.frc.team1002.robot.commands.Lift;
-import org.usfirst.frc.team1002.robot.commands.MoveDistance;
 
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OI {
-    JoystickButton upButton = new JoystickButton(Robot.stick, 6);
-    JoystickButton downButton = new JoystickButton(Robot.stick, 4);
-    JoystickButton gyroResetButton = new JoystickButton(Robot.stick, 2);
+	JoystickButton upButton = new JoystickButton(Robot.stick, 6);
+	JoystickButton downButton = new JoystickButton(Robot.stick, 4);
+	JoystickButton gyroResetButton = new JoystickButton(Robot.stick, 2);
 
-    // TESTING STUFF //
-    JoystickButton encoderMoveButton = new JoystickButton(Robot.stick, 3);
+	public OI() {
+		if (!downButton.get()) {
+			upButton.whileHeld(new Lift(1));
+		}
 
-    public OI() {
-        if (!downButton.get()) upButton.whileHeld(new Lift(1));
+		if (!upButton.get()) {
+			downButton.whileHeld(new Lift(-1));
+		}
 
-        if (!upButton.get()) downButton.whileHeld(new Lift(-1));
+		upButton.whenReleased(new Lift(0));
+		downButton.whenReleased(new Lift(0));
 
-        upButton.whenReleased(new Lift(0));
-        downButton.whenReleased(new Lift(0));
-
-        encoderMoveButton.whenPressed(new MoveDistance(1000));
-
-    }
+	}
 
 }
