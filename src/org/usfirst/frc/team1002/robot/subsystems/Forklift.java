94a657dfdd55bc7d91e98b8e4c1698c52669f691
akilan98 <akilan98@gmail.com>
2015-02-13 16:44:14 -0500

    Fixed limit logic

--- src/org/usfirst/frc/team1002/robot/subsystems/Forklift.java
+++ src/org/usfirst/frc/team1002/robot/subsystems/Forklift.java
@@ -45,7 +45,7 @@ public class Forklift extends Subsystem {
     }
 
     public static void fork(double speed) {
-	if (limitSensorFork.get() && speed < 0) speed = 0;
+	if (!limitSensorFork.get() && speed < 0) speed = 0;
 	forkMotor.set(speed);
     }
 }
