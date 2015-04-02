a2723b8933204005ac3334480dacf38428989d1b
mastercoms <circuitsoft@openmailbox.org>
2015-04-02 10:58:21 -0400

    Update OI.java

--- src/org/usfirst/frc/team1002/robot/OI.java
+++ src/org/usfirst/frc/team1002/robot/OI.java
@@ -9,28 +9,55 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class OI {
     JoystickButton upButton;
     JoystickButton downButton;
-    JoystickButton gyroReset;
-    JoystickButton cartesianToggle;
     JoystickButton forkIn;
     JoystickButton forkOut;
+    
+    JoystickButton upButton2;
+    JoystickButton downButton2;
+    JoystickButton forkIn2;
+    JoystickButton forkOut2;
+    
+    JoystickButton stopDriverTwo;
+    
+	String shouldIStopDriverTwo = null;
 
     public OI(GenericHID... joystick) {
-	this.upButton = new JoystickButton(joystick[0], 5);
-	this.downButton = new JoystickButton(joystick[0], 3);
-	this.forkIn = new JoystickButton(joystick[0], 1);
-	this.forkOut = new JoystickButton(joystick[0], 2);
-
-	// Forklift
-	this.upButton.whileHeld(new Lift(1));
-	this.downButton.whileHeld(new Lift(-1));
-
-	this.upButton.whenReleased(new Lift(0));
-	this.downButton.whenReleased(new Lift(0));
-
-	this.forkIn.whileHeld(new Fork(1));
-	this.forkOut.whileHeld(new Fork(-0.666));
-
-	this.forkIn.whenReleased(new Fork(0));
-	this.forkOut.whenReleased(new Fork(0));
+		upButton = new JoystickButton(joystick[0], 5);
+		downButton = new JoystickButton(joystick[0], 3);
+		forkIn = new JoystickButton(joystick[0], 1);
+		forkOut = new JoystickButton(joystick[0], 2);
+		
+		upButton2 = new JoystickButton(joystick[1], 5);
+		downButton2 = new JoystickButton(joystick[1], 3);
+		forkIn2 = new JoystickButton(joystick[1], 1);
+		forkOut2 = new JoystickButton(joystick[1], 2);
+		
+		stopDriverTwo = new JoystickButton(joystick[0], 12);
+	
+		if (upButton.get() || downButton.get() || forkIn.get() || forkOut.get() || stopDriverTwo.get()) {
+			upButton.whileHeld(new Lift(1));
+			downButton.whileHeld(new Lift(-1));
+	
+			upButton.whenReleased(new Lift(0));
+			downButton.whenReleased(new Lift(0));
+	
+			forkIn.whileHeld(new Fork(1));
+			forkOut.whileHeld(new Fork(-0.666));
+	
+			forkIn.whenReleased(new Fork(0));
+			forkOut.whenReleased(new Fork(0));
+		} else {
+			upButton2.whileHeld(new Lift(1));
+			downButton2.whileHeld(new Lift(-1));
+	
+			upButton2.whenReleased(new Lift(0));
+			downButton2.whenReleased(new Lift(0));
+	
+			forkIn2.whileHeld(new Fork(1));
+			forkOut2.whileHeld(new Fork(-0.666));
+	
+			forkIn2.whenReleased(new Fork(0));
+			forkOut2.whenReleased(new Fork(0));
+		}
     }
 }
