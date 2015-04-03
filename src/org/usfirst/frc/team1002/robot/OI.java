b302ede8bd8d38cf2a4be49c3afd40d5e3f731fc
mastercoms <circuitsoft@openmailbox.org>
2015-04-03 14:42:02 -0400

    update button names, cleanup

--- src/org/usfirst/frc/team1002/robot/OI.java
+++ src/org/usfirst/frc/team1002/robot/OI.java
@@ -8,10 +8,10 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OI {
 	// buttons
-	JoystickButton upButton;
-	JoystickButton downButton;
-	JoystickButton forkIn;
-	JoystickButton forkOut;
+	JoystickButton liftUpButton;
+	JoystickButton liftDownButton;
+	JoystickButton forkInButton;
+	JoystickButton forkOutButton;
 	GenericHID[] joysticks;
 
 	/**
@@ -24,65 +24,62 @@ public class OI {
 		this.joysticks = joysticks;
 
 		// button map
-		upButton = new JoystickButton(joysticks[0], 5);
-		downButton = new JoystickButton(joysticks[0], 3);
-		forkIn = new JoystickButton(joysticks[0], 1);
-		forkOut = new JoystickButton(joysticks[0], 2);
+		liftUpButton = new JoystickButton(joysticks[0], 5);
+		liftDownButton = new JoystickButton(joysticks[0], 3);
+		forkInButton = new JoystickButton(joysticks[0], 1);
+		forkOutButton = new JoystickButton(joysticks[0], 2);
 
 		// button controls
-		upButton.whileHeld(new Lift(1));
-		downButton.whileHeld(new Lift(-1));
+		liftUpButton.whileHeld(new Lift(1));
+		liftDownButton.whileHeld(new Lift(-1));
 
-		upButton.whenReleased(new Lift(0));
-		downButton.whenReleased(new Lift(0));
+		liftUpButton.whenReleased(new Lift(0));
+		liftDownButton.whenReleased(new Lift(0));
 
-		forkIn.whileHeld(new Fork(1));
-		forkOut.whileHeld(new Fork(-0.666));
+		forkInButton.whileHeld(new Fork(1));
+		forkOutButton.whileHeld(new Fork(-0.666));
 
-		forkIn.whenReleased(new Fork(0));
-		forkOut.whenReleased(new Fork(0));
+		forkInButton.whenReleased(new Fork(0));
+		forkOutButton.whenReleased(new Fork(0));
 	}
 
-	/**
-	 *
-	 */
 	public void override() {
 		if (joysticks[0].getRawButton(5) || joysticks[0].getRawButton(3) || joysticks[0].getRawButton(1) || joysticks[0].getRawButton(2) || joysticks[0].getRawButton(12)) {
 			// button map
-			upButton = new JoystickButton(joysticks[0], 5);
-			downButton = new JoystickButton(joysticks[0], 3);
-			forkIn = new JoystickButton(joysticks[0], 1);
-			forkOut = new JoystickButton(joysticks[0], 2);
+			liftUpButton = new JoystickButton(joysticks[0], 5);
+			liftDownButton = new JoystickButton(joysticks[0], 3);
+			forkInButton = new JoystickButton(joysticks[0], 1);
+			forkOutButton = new JoystickButton(joysticks[0], 2);
 
-			upButton.whileHeld(new Lift(1));
-			downButton.whileHeld(new Lift(-1));
+			liftUpButton.whileHeld(new Lift(1));
+			liftDownButton.whileHeld(new Lift(-1));
 
-			upButton.whenReleased(new Lift(0));
-			downButton.whenReleased(new Lift(0));
+			liftUpButton.whenReleased(new Lift(0));
+			liftDownButton.whenReleased(new Lift(0));
 
-			forkIn.whileHeld(new Fork(1));
-			forkOut.whileHeld(new Fork(-0.666));
+			forkInButton.whileHeld(new Fork(1));
+			forkOutButton.whileHeld(new Fork(-0.666));
 
-			forkIn.whenReleased(new Fork(0));
-			forkOut.whenReleased(new Fork(0));
+			forkInButton.whenReleased(new Fork(0));
+			forkOutButton.whenReleased(new Fork(0));
 		} else {
 			// button map
-			upButton = new JoystickButton(joysticks[1], 5);
-			downButton = new JoystickButton(joysticks[1], 3);
-			forkIn = new JoystickButton(joysticks[1], 1);
-			forkOut = new JoystickButton(joysticks[1], 2);
+			liftUpButton = new JoystickButton(joysticks[1], 5);
+			liftDownButton = new JoystickButton(joysticks[1], 3);
+			forkInButton = new JoystickButton(joysticks[1], 1);
+			forkOutButton = new JoystickButton(joysticks[1], 2);
 
-			upButton.whileHeld(new Lift(1));
-			downButton.whileHeld(new Lift(-1));
+			liftUpButton.whileHeld(new Lift(1));
+			liftDownButton.whileHeld(new Lift(-1));
 
-			upButton.whenReleased(new Lift(0));
-			downButton.whenReleased(new Lift(0));
+			liftUpButton.whenReleased(new Lift(0));
+			liftDownButton.whenReleased(new Lift(0));
 
-			forkIn.whileHeld(new Fork(1));
-			forkOut.whileHeld(new Fork(-0.666));
+			forkInButton.whileHeld(new Fork(1));
+			forkOutButton.whileHeld(new Fork(-0.666));
 
-			forkIn.whenReleased(new Fork(0));
-			forkOut.whenReleased(new Fork(0));
+			forkInButton.whenReleased(new Fork(0));
+			forkOutButton.whenReleased(new Fork(0));
 		}
 	}
 }
---
+++ src/org/usfirst/frc/team1002/robot/subsystems/ExtArm.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team1002.robot.subsystems;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ExtArm extends Subsystem {
+
+	VictorSP extMotor;
+
+	public ExtArm() {
+
+	}
+
+	@Override
+	public void initDefaultCommand() {
+
+	}
+}
