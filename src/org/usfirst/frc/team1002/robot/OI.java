ec567dd58b101650ba5eaa44caa330654f521665
mastercoms <circuitsoft@openmailbox.org>
2015-04-03 10:59:03 -0400

    override for forklift

--- src/org/usfirst/frc/team1002/robot/OI.java
+++ src/org/usfirst/frc/team1002/robot/OI.java
@@ -29,22 +29,46 @@ public class OI {
 		forkIn = new JoystickButton(joysticks[1], 1);
 		forkOut = new JoystickButton(joysticks[1], 2);
 		
-		// button controls
-		upButton.whileHeld(new Lift(1));
-		downButton.whileHeld(new Lift(-1));
-
-		upButton.whenReleased(new Lift(0));
-		downButton.whenReleased(new Lift(0));
-
-		forkIn.whileHeld(new Fork(1));
-		forkOut.whileHeld(new Fork(-0.666));
-
-		forkIn.whenReleased(new Fork(0));
-		forkOut.whenReleased(new Fork(0));566666666666666666666665555
     }
     
     public void override() {
-    	if (joysticks[0].getRawButton(5) || joysticks[0].getRawButton() || joysticks[0].getRawButton(5) || )
-    	upButton = new JoystickButton(joysticks[0], 5);
+    	if (joysticks[0].getRawButton(5) || joysticks[0].getRawButton(3) || joysticks[0].getRawButton(1) || joysticks[0].getRawButton(2)) {
+    		// button map
+    		upButton = new JoystickButton(joysticks[0], 5);
+    		downButton = new JoystickButton(joysticks[0], 3);
+    		forkIn = new JoystickButton(joysticks[0], 1);
+    		forkOut = new JoystickButton(joysticks[0], 2);
+
+    		upButton.whileHeld(new Lift(1));
+    		downButton.whileHeld(new Lift(-1));
+
+    		upButton.whenReleased(new Lift(0));
+    		downButton.whenReleased(new Lift(0));
+
+    		forkIn.whileHeld(new Fork(1));
+    		forkOut.whileHeld(new Fork(-0.666));
+
+    		forkIn.whenReleased(new Fork(0));
+    		forkOut.whenReleased(new Fork(0));
+    	} else {
+    		// button map
+    		upButton = new JoystickButton(joysticks[1], 5);
+    		downButton = new JoystickButton(joysticks[1], 3);
+    		forkIn = new JoystickButton(joysticks[1], 1);
+    		forkOut = new JoystickButton(joysticks[1], 2);
+
+    		upButton.whileHeld(new Lift(1));
+    		downButton.whileHeld(new Lift(-1));
+
+    		upButton.whenReleased(new Lift(0));
+    		downButton.whenReleased(new Lift(0));
+
+    		forkIn.whileHeld(new Fork(1));
+    		forkOut.whileHeld(new Fork(-0.666));
+
+    		forkIn.whenReleased(new Fork(0));
+    		forkOut.whenReleased(new Fork(0));
+    	}
+    	
     }
 }
