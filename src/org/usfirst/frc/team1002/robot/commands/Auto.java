32e6c9252e9fa5da7085861e04fa162bc739430f
mastercoms <circuitsoft@openmailbox.org>
2015-04-24 13:56:43 -0500

    best auto 8/8

--- src/org/usfirst/frc/team1002/robot/commands/Auto.java
+++ src/org/usfirst/frc/team1002/robot/commands/Auto.java
@@ -27,8 +27,29 @@ public class Auto extends CommandGroup {
 				addSequential(new Move(0, 0.25, 0), 1);
 				break;
 			case 4:
-				addSequential(new Move(-0.25, 0, 0.5), 1);
-				addSequential(new ExtArm(1), 1);
+				// 1st container and tote
+				addSequential(new Move(-0.5, 0, -0.5), 1);
+				addSequential(new ExtArm(1), 0.5);
+				addSequential(new Move(0.5, 0, 0.5), 1);
+				addSequential(new ExtArm(-1), 0.5);
+				
+				// container 2
+				addSequential(new Move(-0.5, 0, -0.5), 1);
+				addSequential(new ExtArm(1), 0.5);
+				addSequential(new Move(0.5, 0, 0.5), 1);
+				addSequential(new ExtArm(-1), 0.5);
+				
+				// container 3
+				addSequential(new Move(-0.5, 0, -0.5), 1);
+				addSequential(new ExtArm(1), 0.5);
+				addSequential(new Move(0.5, 0, 0.5), 1);
+				addSequential(new ExtArm(-1), 0.5);
+				
+				// move towards step container 1
+				addSequential(new Move(-0.5, 0, -0.5), 1);
+				addSequential(new Move(0, -0.5, 0), 1);
+				addSequential(new ExtArm(1), 0.5);
+				addSequential(new ExtArm(-1), 0.5);
 				break;
 			default:
 				addSequential(new Move(0, -0.25, 0), 1);
